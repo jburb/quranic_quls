eb5bf0ac03a09e490cf2520a257011b49999b401
Josh Hicken <jhicken@pm.me>
2024-01-31 02:43:10 -0800

    updating notes in poc module. major realization that qurancorpus lib seems to support only py2.x

--- docs/proof_of_concept.py
+++ docs/proof_of_concept.py
@@ -2,3 +2,9 @@
 # TODO create draft of using tajweed pauses to infer qul end markings
 
 # TODO Consider how the output of above can actually be used to parse corresponding statements from translated ayahs
+import os
+os.chdir('data')
+
+import qurancorpus
+
+''' NOTE: Important! This qurancorpus lib appears to support only py2.x. Hence it will have to be executed with same.'''
