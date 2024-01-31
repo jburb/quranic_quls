a1e7c0f23eea859c7011c708569a3b1cb506b305
Josh Hicken <jhicken@pm.me>
2024-01-31 01:55:07 -0800

    adding near-empty poc file, updating pdf with latest chat suggestions added

---
+++ docs/proof_of_concept.py
@@ -0,0 +1,4 @@
+# TODO create draft of using quran corpus to find qul starting verses
+# TODO create draft of using tajweed pauses to infer qul end markings
+
+# TODO Consider how the output of above can actually be used to parse corresponding statements from translated ayahs
